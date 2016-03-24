7ddc62ecfe7a3a978d73ddfb4bfc0a619420f052
ShiroJun <0909robinson@gimail.com>
2016-03-24 15:54:38 +0900

    suzuki パズル部分のスクリプト追加

---
+++ Assets/Suzuki/Scripts/CollectionButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CollectionButton : MonoBehaviour
+{
+    public Text memoPos;
+    public GameObject sushiPos;
+
+    public void OnClick()
+    {
+        Debug.Log(gameObject.name);
+
+        Sprite[] images = Resources.LoadAll<Sprite>("sb");
+        //Texture2D texture = Resources.Load("sb_"+ gameObject.name) as Texture2D;
+        Image img = sushiPos.GetComponent<Image>();
+        img.sprite = images[int.Parse(gameObject.name)];
+        //Debug.Log(Resources.Load("sb_" + gameObject.name));
+    }
+
+}
---
+++ Assets/Suzuki/Scripts/SceneLoads.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoads : MonoBehaviour {
+	public void SceneLoadCollection()
+	{
+		SceneManager.LoadScene("Collection");
+	}
+	public void SceneLoadMaim()
+	{
+		SceneManager.LoadScene("Main");
+	}
+}
