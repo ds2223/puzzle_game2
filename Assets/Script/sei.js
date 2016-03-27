8041c129911be4c6665379b1908f94d9056a5c38
nemoto <x0703x@gmail.com>
2016-03-28 00:04:48 +0900

    パワーポイント作成

--- Assets/Script/sei.js
+++ Assets/Script/sei.js
@@ -227,11 +227,11 @@ private function DropBall(count: int) {
 		ball.transform.position.y = 7; 							//ボールのｙ座標を調整
 		ball.transform.eulerAngles.z = Random.Range(-40, 40); 	//ボールの角度をランダムに設定
 		var spriteId: int = Random.Range(0, 5); 				//ボールの画像のid(ボールの色)をランダムに設定
-		//ball.name = "Ball" + spriteId; 							//ボールの名前を画像のidに合わせ変更
-		ball.name = "Ball0"; 
+		ball.name = "Ball" + spriteId; 							//ボールの名前を画像のidに合わせ変更
+		
 		var ballTexture = ball.GetComponent(SpriteRenderer); 	//ボールの画像を管理している要素を取得
-		//ballTexture.sprite = ballSprites[spriteId]; 			//ボールの画像をidに合わせて変更
-		ballTexture.sprite = ballSprites[0]; 
+		ballTexture.sprite = ballSprites[spriteId]; 			//ボールの画像をidに合わせて変更
+		
 		yield WaitForSeconds(0.05); 							//次のボールを生成するまで一定時間待つ
 	}
 }
